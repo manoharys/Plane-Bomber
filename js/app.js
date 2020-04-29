9ac25b17a50e93640bc5b736126720639cd25af4
manohar <manumanohar1527@gmail.com>
2020-04-29 20:43:55 +0530

    'start Game'

--- js/app.js
+++ js/app.js
@@ -6,10 +6,19 @@ const message = document.querySelector('.gameMessage');
 keys = {
     space : false
 }
+//Object which tracks the player gameplay dynamically
+player = {
+    inplay : false, 
+    score : 0,
+    speed :2
+}
 
 //keyBoard events
 document.addEventListener('keydown',pressOn);
 document.addEventListener('keyup',pressOff);
+
+//click event to start the Game.
+document.addEventListener('click',start);
 
 //function which gives the key pressed info
 function pressOn(e){
@@ -23,4 +32,12 @@ function pressOff(e){
     let tempKeys = (e.key == " ")?"space":e.key;
     keys[tempKeys] = false;
     console.log(keys)
+}
+
+//Function which starts the game
+function start(){
+   inplay = true;
+   player.plane = document.createElement('div');
+   player.plane.classList.add('plane');
+   gameArea.appendChild(player.plane);
 }
